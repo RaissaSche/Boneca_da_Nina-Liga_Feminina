a92e21fea89e42f2a5010459beef732b724e5e6f
Raissa Scheeren <raissa.fofs@gmail.com>
2019-11-04 23:20:08 -0300

    adding sadness level

---
+++ A Boneca da Nina - project/Assets/Scripts/PlayerBoat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBoat : MonoBehaviour {
+	
+	public float offset = 0f;
+	private Vector3 initialPosition;
+
+	public void ClickUp() {
+		Debug.Log ("ClickUp");
+		if (transform.position.y < initialPosition.y + offset) {
+			transform.localPosition += new Vector3(0, offset);
+		}
+	}
+
+	public void ClickDown() {
+		Debug.Log ("ClickDown " + (initialPosition.y - offset).ToString());
+		if (transform.position.y > initialPosition.y - offset) {
+			transform.localPosition -= new Vector3(0, offset);
+		}
+	}
+
+	void Update () {
+
+	
+	}
+}
