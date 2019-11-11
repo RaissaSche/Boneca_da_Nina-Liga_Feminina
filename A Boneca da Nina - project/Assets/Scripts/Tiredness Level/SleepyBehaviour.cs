579f4f15c8a1c4b614323135fbebb13838563061
Joao Pedro Bohn <JPBOHN@edu.unisinos.br>
2019-11-11 14:07:39 -0200

    added tiredness level

---
+++ A Boneca da Nina - project/Assets/Scripts/Tiredness Level/SleepyBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Status : byte
+{
+    SLEEPING = 0,
+    WAKINGUP = 1,
+    AWAKEN = 2,
+    FALLINGASLEEP = 3
+};
+
+public class SleepyBehaviour : MonoBehaviour
+{
+    [SerializeField] private Status status;
+    private Transform player;
+
+
+
+    private void setStatus(Status sta)
+    {
+        status = sta;
+    }
+
+    private void setStatusAwaken()
+    {
+        setStatus(Status.AWAKEN);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = Status.SLEEPING;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject.transform;
+            status = Status.WAKINGUP;
+            Invoke("setStatusAwaken", 1.0f);
+        }
+    }
+}
