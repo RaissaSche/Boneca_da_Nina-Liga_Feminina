4fa1af8c58a33b7fd30fd76b84849e7d42a5b8a2
Julia Ferreira Vila <JULIAFV@edu.unisinos.br>
2019-11-18 14:10:27 -0200

    wip

--- A Boneca da Nina - project/Assets/Scripts/BoatMovement.cs
+++ A Boneca da Nina - project/Assets/Scripts/BoatMovement.cs
@@ -2,39 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoatMovement : MonoBehaviour {
-	
-	private float RotateSpeed = 6f;           
-	private float _angle;
-	private int Direction = 1;
+public class BoatMovement : MonoBehaviour
+{
 
-	void Start () {
-		transform.eulerAngles = new Vector3 (0,0,0);
-	}
-	
-	void Update () {
+    private float RotateSpeed = 6f;
+    private float _angle;
+    private int Direction = 1;
+    private bool inBossPlace = false;
 
-		if (Direction > 0) {
-			if (transform.eulerAngles.z > 350 && transform.eulerAngles.z <= 357 && RotateSpeed > 1f)
-				RotateSpeed -= .2f;
-			else if (Direction > 0 && transform.eulerAngles.z >= 340 && transform.eulerAngles.z <= 350) {
-				Direction = -1;
-				RotateSpeed = 0;
-			}
-		} 
-		else 
-		{
-			if (transform.eulerAngles.z > 3 && transform.eulerAngles.z < 10 && RotateSpeed > 1f)
-				RotateSpeed -= .2f;
-			else if (transform.eulerAngles.z <= 20 && transform.eulerAngles.z >= 10) {
-				Direction = 1;
-				RotateSpeed = 0;
-			}
-		}
-		
-		transform.Rotate (Vector3.forward *- (5 * Direction * RotateSpeed * Time.deltaTime));
-		RotateSpeed += .07f;
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
 
-		transform.position += new Vector3 (10 * Time.deltaTime, 0, 0);
-	}
+    void Update()
+    {
+        if (Direction > 0)
+        {
+            if (transform.eulerAngles.z > 350 && transform.eulerAngles.z <= 357 && RotateSpeed > 1f)
+                RotateSpeed -= .2f;
+            else if (Direction > 0 && transform.eulerAngles.z >= 340 && transform.eulerAngles.z <= 350)
+            {
+                Direction = -1;
+                RotateSpeed = 0;
+            }
+        }
+        else
+        { 
+            if (transform.eulerAngles.z > 3 && transform.eulerAngles.z < 10 && RotateSpeed > 1f)
+                RotateSpeed -= .2f;
+            else if (transform.eulerAngles.z <= 20 && transform.eulerAngles.z >= 10)
+            {
+                Direction = 1;
+                RotateSpeed = 0;
+            }
+        }
+
+        transform.Rotate(Vector3.forward * -(5 * Direction * RotateSpeed * Time.deltaTime));
+        RotateSpeed += .07f;
+    }
 }
